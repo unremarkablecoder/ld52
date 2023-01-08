3ee4c4b460fda72949c70460b44c9c9483541586
Pontus Eriksson <pontus.eriksson@gmail.com>
2023-01-08 19:41:03 +0100

    another level. lower guard speed

--- Assets/GameLoop.cs
+++ Assets/GameLoop.cs
@@ -16,17 +16,29 @@ public class GameLoop : MonoBehaviour {
 
     private bool levelWasLoaded = false;
     private int loadedLevelIndex = 0;
+    private bool inLevel = false;
 
     void Awake() {
         audioManager = FindObjectOfType<AudioManager>();
     }
     
     private void Update() {
-        if (Input.GetKeyUp(KeyCode.Alpha1)) {
-           LoadLevel(1); 
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha2)) {
-           LoadLevel(2); 
+        if (!inLevel) {
+            if (Input.GetKeyUp(KeyCode.Alpha1)) {
+                LoadLevel(1);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Alpha2)) {
+                LoadLevel(2);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Alpha3)) {
+                LoadLevel(3);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Alpha4)) {
+                LoadLevel(4);
+            }
         }
 
         if (levelWasLoaded) {
@@ -45,6 +57,7 @@ public class GameLoop : MonoBehaviour {
     }
 
     public void LoadLevel(int level) {
+        inLevel = true;
         winScreen.SetActive(false);
         dieScreen.SetActive(false);
         levelSelectScreen.SetActive(false);
@@ -60,6 +73,7 @@ public class GameLoop : MonoBehaviour {
         loadedLevelIndex = 0;
         blood.Clear();
         corpses.Clear();
+        inLevel = false;
     }
     
     public void Die() {
